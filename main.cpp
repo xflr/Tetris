bb7122478a41b16ee1aae79985c7c63a9fef73cf
Alexandre Bressane <alexandre.bressane@hotmail.com>
2021-04-15 15:04:30 -0300

    Boundaries done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <SDL2/SDL_scancode.h>
 #include <SDL2/SDL_ttf.h>
 #include <iostream>
-#include <string>
 #include <stdio.h>
 
 using namespace std;
@@ -13,6 +12,7 @@ const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 
 SDL_Rect Message_rect;
+
 TTF_Font* font;
 
 wstring tetromino[7];
@@ -20,6 +20,46 @@ wstring tetromino[7];
 int nFieldWidth = 12; //game field width
 int nFieldHeight = 18; //game field height
 unsigned char *pField = nullptr; //initiate field as null pointer
+
+string isOnBoundaries(int cDirection) 
+{
+    switch (cDirection)
+    {
+    case 0:
+        if ( Message_rect.x <= 0 )
+        { 
+            return "left_bound"; 
+            break;
+        }
+        return " ";
+        break;
+    case 1:
+        if ( (Message_rect.x + Message_rect.w) >= SCREEN_WIDTH)
+        { 
+            return "right_bound"; 
+            break;
+        }
+        return " ";
+        break;
+    
+    case 2:
+        if (Message_rect.y <= 0)
+        { 
+            return "top_bound"; 
+            break;
+        }
+        return " ";
+        break;
+    case 3:
+        if ( (Message_rect.y + Message_rect.h) >= SCREEN_HEIGHT)
+        { 
+            return "bottom_b"; 
+            break;    
+        }
+        return " ";
+        break;
+    }
+}
 
 SDL_Color foregroundColor = { 255, 255, 255 };
 SDL_Color backgrounddColor = { 0, 0, 0 };
@@ -51,16 +91,14 @@ int main ( int argc, char **argv )
         printf("ERROR TTF");
     }
     
-    font = TTF_OpenFont("couri.ttf", 12);
+    font = TTF_OpenFont("Courier_New.ttf", 32);
+    
     if (!font)
     {
         printf("ERRO FONTE %s\n", TTF_GetError());
     }
-    SDL_Surface* textSurface = TTF_RenderText_Solid(font, "CHAMAAAAAAAAAAAAAAAAAAAAAAAA", foregroundColor);       
-
-    
-    
-
+    SDL_Surface* textSurface = TTF_RenderText_Shaded(font, "CHAMA!", foregroundColor, { 255, 50, 0 });       
+    SDL_Surface* leftBoundSurface = TTF_RenderText_Shaded(font, "#", foregroundColor, { 255, 255, 0 });
     if ( SDL_Init ( SDL_INIT_VIDEO ) < 0)
     {
         printf( "SDL coud not intilalize! SDL_Error: %s\n", SDL_GetError() );
@@ -69,21 +107,19 @@ int main ( int argc, char **argv )
     {
         window = SDL_CreateWindow( "TETRIS by Alexandre Bressane - press q to quit", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0 );
     }
-    
 
     Message_rect.x = 20;
     Message_rect.y = 10;
-    Message_rect.w = 800;
-    Message_rect.h = 140;
+    Message_rect.w = 480;
+    Message_rect.h = 240;
     
     if ((screen = SDL_CreateRenderer(window, -1, 0)) < 0)
     {
         printf("Error creating renderer %s\n", SDL_GetError());
     } 
+    
     Message = SDL_CreateTextureFromSurface(screen, textSurface);
     
-    
-
     SDL_Event e;
 
     //tetris blocks as assets in tetromino matrix
@@ -142,9 +178,6 @@ int main ( int argc, char **argv )
         }
         else
         {
-            
-            
-            
             SDL_SetRenderDrawColor(screen, 50, 0, 255, 255);
             SDL_RenderClear(screen);
 
@@ -154,9 +187,9 @@ int main ( int argc, char **argv )
             }
             SDL_RenderPresent(screen);
 
-            SDL_Delay( 200 );
+            SDL_Delay( 17 );
 
-            Message_rect.x++;
+            
             if (state[SDL_SCANCODE_Q])
             {
                 SDL_FreeSurface(textSurface);
@@ -165,6 +198,47 @@ int main ( int argc, char **argv )
                 SDL_Quit();
                 bGameOver = true;
                 return(0);
+            }
+
+            if (state[SDL_SCANCODE_LEFT])
+            {
+                // keep it here to know we can change the text/texture in the gameloop
+                //SDL_Surface* textSurface = TTF_RenderText_Solid(font, "PAHHHH", foregroundColor);
+                //Message = SDL_CreateTextureFromSurface(screen, textSurface);
+                if (!(isOnBoundaries(0) == "left_bound"))
+                {
+                    Message_rect.x--;
+                }
+            } 
+            if (state[SDL_SCANCODE_RIGHT])
+            {
+                // keep it here to know we can change the text/texture in the gameloop
+                //SDL_Surface* textSurface = TTF_RenderText_Solid(font, "PAHHHH", foregroundColor);
+                //Message = SDL_CreateTextureFromSurface(screen, textSurface);
+                if (!(isOnBoundaries(1) == "right_bound"))
+                {
+                    Message_rect.x++;
+                }
+            } 
+            if (state[SDL_SCANCODE_UP])
+            {
+                // keep it here to know we can change the text/texture in the gameloop
+                //SDL_Surface* textSurface = TTF_RenderText_Solid(font, "PAHHHH", foregroundColor);
+                //Message = SDL_CreateTextureFromSurface(screen, textSurface);
+                if (!(isOnBoundaries(2) == "top_bound"))
+                {
+                    Message_rect.y--;
+                }
+            }
+            if (state[SDL_SCANCODE_DOWN])
+            {
+                // keep it here to know we can change the text/texture in the gameloop
+                //SDL_Surface* textSurface = TTF_RenderText_Solid(font, "PAHHHH", foregroundColor);
+                //Message = SDL_CreateTextureFromSurface(screen, textSurface);
+                if (!(isOnBoundaries(3) == "bottom_b"))
+                {    
+                    Message_rect.y++;
+                }
             } 
         }
 
